a700cbad0a259243ceaa5bbec690ddb9b75f2bbb
Chandra Nguyen <chandra.nguyen@outlook.com>
2020-09-10 17:02:39 -0600

    Commented all scripts

--- server/index.js
+++ server/index.js
@@ -6,19 +6,6 @@ const port = 3000;
 
 app.use('/:id', express.static('public'));
 
-// // Photo Carousel
-// app.use('/api/photos/:restaurantID', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
-
-// // Info Sidebar
-// app.use('/:copyId/restaurants/:id', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true }));
-
-// // Tips & Recommendations
-// app.use('/api/tips/:id', createProxyMiddleware({ target: 'http://localhost:6070', changeOrigin: true }));
-// app.use('/api/articles/:id', createProxyMiddleware({ target: 'http://localhost:6070', changeOrigin: true }));
-
-// // Similar Restaurants
-// app.use('/restaurants/:id', createProxyMiddleware({ target: 'http://localhost:3004', changeOrigin: true }));
-
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
