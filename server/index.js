0e06b807d4dde735fd86aae1dcd8db38305847e3
Chandra Nguyen <chandra.nguyen@outlook.com>
2020-09-11 17:32:54 -0600

    attempting to set public as static folder for loader

--- server/index.js
+++ server/index.js
@@ -4,8 +4,10 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const port = 3000;
 
-app.use('/:id', express.static('public'));
+
 app.use(express.static('public'));
+app.use('/:id', express.static('public'));
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
